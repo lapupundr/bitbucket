d7298b7ae2e4b984ef2f13de8bc2c2563509d43e
Tatsiana Zatonets <koshka.tan@gmail.com>
2022-11-01 18:16:44 +0300

    install twig, add template and style.css

--- app/Test.php
+++ app/Test.php
@@ -4,11 +4,20 @@ declare(strict_types=1);
 
 namespace Auth;
 
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
 class Test
 {
     public function execute(): void
     {
         echo('TEST');
+        $loader = new FilesystemLoader('templates');
+        $twig = new Environment(
+            $loader
+        );
+        echo $twig->render('mainAuth.twig');
+
     }
 }
 
