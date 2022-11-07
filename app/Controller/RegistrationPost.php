fc5ad4475dcb90ee5614c0e1d21c507cb41bc96a
Tatsiana Zatonets <koshka.tan@gmail.com>
2022-11-07 20:52:37 +0300

    reformat code

--- app/Controller/RegistrationPost.php
+++ app/Controller/RegistrationPost.php
@@ -12,37 +12,35 @@ class RegistrationPost implements ControllerInterface
      */
     public function execute(): void
     {
-        if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest') {
-            if (isset($_POST["login"]) && isset($_POST["pass"])) {
-                $userId = $_SESSION['userId'];
-                $userArr = new ReadData();
-                $userArr = $userArr->execute('app/db/user.json');
-                if (isset($userArr[$userId])) {
-                    $result = json_encode($userArr[$userId]);
-                    echo $result;
-//                    header("Location: /");
-                } else {
-                    $result = [
-                        'login'        => $_POST["login"],
-                        'pass'         => $_POST["pass"],
-                        'pass_confirm' => $_POST['pass_confirm'],
-                        'mail'         => $_POST['mail'],
-                        'name'         => $_POST['name'],
-                    ];
-                    $userArr[$userId] = $result;
+        if (isset($_SERVER['HTTP_X_REQUESTED_WITH'])
+            && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest'
+            && isset($_POST["login"])
+            && isset($_POST["pass"])
+        ) {
+            $userId = $_SESSION['userId'];
+            $userArr = new ReadData();
+            $userArr = $userArr->execute('app/db/user.json');
+            if (isset($userArr[$userId])) {
+                $result = json_encode($userArr[$userId]);
+                echo $result;
+                //                    header("Location: /");
+            } else {
+                $result = [
+                    'login'        => $_POST["login"],
+                    'pass'         => $_POST["pass"],
+                    'pass_confirm' => $_POST['pass_confirm'],
+                    'mail'         => $_POST['mail'],
+                    'name'         => $_POST['name'],
+                ];
+                $userArr[$userId] = $result;
 
-                    //                $user = new CheckData();
-                    //                $data = $result['id_user'];
-                    //                $user->execute('app/db/user.json', $data);
+                $userJson = json_encode($userArr);
+                $updateData = new UpdateData();
+                $updateData->execute('app/db/user.json', $userJson);
 
-                    $userJson = json_encode($userArr);
-                    $updateData = new UpdateData();
-                    $updateData->execute('app/db/user.json', $userJson);
-
-                    $result = json_encode($result);
-                    header('Content-Type: application/json');
-                    echo $result;
-                }
+                $result = json_encode($result);
+                header('Content-Type: application/json');
+                echo $result;
             }
         } else {
             header("Location: /");
