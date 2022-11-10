32ac2c6c433c13a0cf2df8ee1c45b99277f231b8
Tatsiana Zatonets <koshka.tan@gmail.com>
2022-11-11 01:29:37 +0300

    add condition into class RegistrationPost

--- app/Controller/RegistrationPost.php
+++ app/Controller/RegistrationPost.php
@@ -20,7 +20,7 @@ class RegistrationPost implements ControllerInterface
             $userId = $_SESSION['userId'];
             $userArr = new ReadData();
             $userArr = $userArr->execute('app/db/user.json');
-            if (isset($userArr[$userId])) {
+            if (isset($userArr[$userId]) && $userArr[$userId]['login'] === $_POST['login']) {
                 $result = json_encode($userArr[$userId]);
             } else {
                 $result = [
