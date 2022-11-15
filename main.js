8eba4b39998d30e1c412d2bca02e7927f2f47233
Tatsiana Zatonets <koshka.tan@gmail.com>
2022-11-15 13:45:56 +0300

    develop js validation

--- main.js
+++ main.js
@@ -1,52 +1,87 @@
 // "use strict";
-const form  = document.getElementsByTagName('form')[0];
+const form = document.getElementsByTagName('form')[0];
+const login = document.getElementById('login');
+const password = document.getElementById('pass');
+const pass_confirm = document.getElementById('pass_confirm');
 const email = document.getElementById('mail');
+const name = document.getElementById('name');
+const loginError = document.querySelector('#login + span.error');
 const emailError = document.querySelector('#mail + span.error');
+const nameError = document.querySelector('#name + span.error');
+$('#btn_reg').prop('disabled', true);
+let errorData = true;
 
-email.addEventListener('input', function (event) {
-    console.log('valid email input');
+form.addEventListener('input', function (event) {
+    if (errorData) {
+        $('#btn_reg').prop('disabled', true);
+    } else {
+        $('#btn_reg').prop('disabled', false);
+    }
+})
 
-    if (email.validity.valid) {
-        emailError.textContent = '';
-        emailError.className = 'error';
+login.addEventListener('input', function (event) {
+    if (login.validity.valid) {
+        loginError.textContent = '';
+        loginError.className = 'error';
+        errorData = false;
     } else {
         showError();
     }
 });
 
-form.addEventListener('submit', function (event) {
-    // Если поле email валдно, позволяем форме отправляться
-    console.log('valid email');
+email.addEventListener('input', function (event) {
+    if (email.validity.valid) {
+        emailError.textContent = '';
+        emailError.className = 'error';
+        errorData = false;
+    } else {
+        showEmailError();
+    }
+});
 
-    if (!email.validity.valid) {
-        // Если поле email не валидно, отображаем соответствующее сообщение об ошибке
-        showError();
-        // Затем предотвращаем стандартное событие отправки формы
-        event.preventDefault();
+name.addEventListener('input', function (event) {
+    if (name.validity.valid) {
+        nameError.textContent = '';
+        nameError.className = 'error';
+        errorData = false;
+    } else {
+        showNameError();
     }
 });
 
 function showError() {
+    errorData = true;
+    if (login.validity.valueMissing) {
+        loginError.textContent = 'You need to enter a login.';
+    } else if (login.validity.tooShort) {
+        loginError.textContent = `Login should be at least ${login.minLength} characters; you entered ${login.value.length}.`;
+    }
+    loginError.className = 'error active';
+}
+
+function showEmailError() {
+    errorData = true;
     if (email.validity.valueMissing) {
-        // Если поле пустое,
-        // отображаем следующее сообщение об ошибке
         emailError.textContent = 'You need to enter an e-mail address.';
     } else if (email.validity.typeMismatch) {
-        // Если поле содержит не email-адрес,
-        // отображаем следующее сообщение об ошибке
         emailError.textContent = 'Entered value needs to be an e-mail address.';
     } else if (email.validity.tooShort) {
-        // Если содержимое слишком короткое,
-        // отображаем следующее сообщение об ошибке
         emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
     }
-
-    // Задаём соответствующую стилизацию
     emailError.className = 'error active';
 }
 
+function showNameError() {
+    errorData = true;
+    if (name.validity.valueMissing) {
+        nameError.textContent = 'You need to enter a name.';
+    }else if (name.validity.patternMismatch){
+        nameError.textContent = 'Name should contain only letters more than 2'
+    }
+    nameError.className = 'error active';
+}
+
 $(document).ready(function () {
-    // $('#btn_reg').prop('disabled', true);
     $("#btn_reg").click(
         function () {
             console.log('Нажата submit-кнопка');
